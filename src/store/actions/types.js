689e539906c20129e0057c0bf1e9cd1a4b69b87d
tanamet.t <tanamet.tpk@gmail.com>
2019-09-11 18:34:49 +0700

    + types for actions

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,2 +1,7 @@
 export const FETCH_POST = "FETCH_POST"
 export const ADD_POST = "ADD_POST"
+export const UPDATE_POST = "UPDATE_POST"
+export const DELETE_POST = "DELETE_POST"
+
+export const REGISTER = "REGISTER"
+export const GET_USER = "GET_USER"
